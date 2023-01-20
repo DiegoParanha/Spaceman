e21cd0980c6c39da47406e106c3129d0fa3b17e3
DiegoParanha <DiegoParanhos14@gmail.com>
2023-01-20 15:18:54 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* constants */
+
+
+/* App’s States (variables) */
+
+
+/* Cached Element References */
+
+
+/* Event Listeners */
+
+
+/* Functions */
+init();
+
+
+function init() {
+
+
+
+    render();
+}
