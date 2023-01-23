27d5d5ac28185920c07a9228a14c8555217f3cd6
DiegoParanha <DiegoParanhos14@gmail.com>
2023-01-23 11:46:49 -0800

    third commit/ board is coming together

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 /* constants */
-const wordChoice = {
-   words: ['sun', 'moon', 'earth', 'jupiter', 'saturn', 'ufo', 'pluto', 'mars', 'uranus', 'venus', 'spaceman', 'mercury'],
- };
+const wordChoice = [
+  'sun', 
+  'moon', 
+  'earth', 
+  'jupiter', 
+  'saturn', 
+  'ufo', 
+  'pluto', 
+  'mars', 
+  'uranus', 
+  'venus', 
+  'spaceman', 
+  'mercury'
+]
+
 
 /* App’s States (variables) */
-let wordBox;
+let answer = '';
+let mistakes = 0;
+let maxWrong = 6;
+let guessed = [];
+let wordStatus = null;
 
 /* Cached Element References */
 
@@ -23,6 +39,33 @@ function init() {
     render();
 }
 
+
 function render() {
 
 };
+
+function generateWord() {
+  answer = wordChoice[Math.floor(Math.random() * wordChoice.length)];
+}
+
+
+function wordBox() {
+  let wordsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').join(' ').toUpperCase();
+
+  document.getElementById('keyboard').innerHTML = wordsHTML
+}
+
+
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('wordSpotLight').innerHTML = wordStatus
+}
+
+guessedWord();
+generateWord();
+wordBox();
+
+
+
+
