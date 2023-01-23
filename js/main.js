589560c8ae8604b184c42e6d58f736cf82f19e49
DiegoParanha <DiegoParanhos14@gmail.com>
2023-01-23 08:59:43 -0800

    second commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 /* constants */
-
+const wordChoice = {
+   words: ['sun', 'moon', 'earth', 'jupiter', 'saturn', 'ufo', 'pluto', 'mars', 'uranus', 'venus', 'spaceman', 'mercury'],
+ };
 
 /* App’s States (variables) */
-
+let wordBox;
 
 /* Cached Element References */
 
@@ -20,3 +22,7 @@ function init() {
 
     render();
 }
+
+function render() {
+
+};
